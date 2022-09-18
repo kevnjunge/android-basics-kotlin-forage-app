57897a4cff4327b05b18058d9e3ef13a5c574f1f
kevNjunge <njungechuguka@gmail.com>
2022-09-18 15:48:32 +0300

    database class implementation

--- app/src/main/java/com/example/forage/data/ForageDatabase.kt
+++ app/src/main/java/com/example/forage/data/ForageDatabase.kt
@@ -15,6 +15,10 @@
  */
 package com.example.forage.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.forage.model.Forageable
 
@@ -22,5 +26,26 @@ import com.example.forage.model.Forageable
  * Room database to persist data for the Forage app.
  * This database stores a [Forageable] entity
  */
-// TODO: create the database with all necessary annotations, methods, variables, etc.
-abstract class ForageDatabase : RoomDatabase()
+@Database(entities = [Forageable::class], version = 1, exportSchema = false)
+abstract class ForageDatabase : RoomDatabase(){
+
+    abstract fun foregiableDao():ForageableDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ForageDatabase? = null
+        fun getDatabase(context: Context):ForageDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                   ForageDatabase::class.java,
+                   "forgeable_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
